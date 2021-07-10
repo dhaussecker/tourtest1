5aec3506f69b8172325aa94ca96435b2c781891e
djh293 <48139207+djh293@users.noreply.github.com>
2021-07-09 22:42:43 -0600

    first commit

---
+++ app-files/data.js
@@ -0,0 +1,47 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-16510758704_64ac808b98_b",
+      "name": "16510758704_64ac808b98_b",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.42877900669528657,
+          "pitch": 0.3879661192916153,
+          "rotation": 0,
+          "target": "0-16510758704_64ac808b98_b"
+        },
+        {
+          "yaw": 0.42878156699528347,
+          "pitch": 0.3879635589916184,
+          "rotation": 0,
+          "target": "0-16510758704_64ac808b98_b"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
